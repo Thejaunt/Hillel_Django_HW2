23bb5252bf76557fe25f1f231f82a917d48974b9
Sam <119899718+Thejaunt@users.noreply.github.com>
2023-06-24 04:42:48 +0300

    Update views.py

--- triangle/views.py
+++ triangle/views.py
@@ -8,14 +8,12 @@ from .models import Person
 
 def find_hypotenuse(request):
     form = TriangleForm(request.POST or None)
-
     if request.method == "POST":
         if form.is_valid():
             cath1 = form.cleaned_data.get("cath1")
             cath2 = form.cleaned_data.get("cath2")
             res = round(float(cath1**2 + cath2**2) ** 0.5, 2)
             return render(request, "form.html", {"form": form, "res": res})
-
     return render(request, "form.html", {"form": form})
 
 
