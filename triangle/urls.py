d171f9186a5e40f68071901fc28d58390d52f9a0
Thejaunt <thejauntthejaunt@gmail.com>
2023-06-24 22:13:48 +0300

    crud update

--- triangle/urls.py
+++ triangle/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import create_person, find_hypotenuse, list_persons_view, person_detail_view, update_person
+from .views import create_person, delete_person, find_hypotenuse, list_persons_view, person_detail_view, update_person
 
 urlpatterns = [
     path("", find_hypotenuse, name="find_hypotenuse"),
@@ -8,4 +8,5 @@ urlpatterns = [
     path("person/<int:pk>", update_person, name="update_person"),
     path("persons-list", list_persons_view, name="list_persons"),
     path("person-detail/<int:pk>", person_detail_view, name="person-detail"),
+    path("delete-person/<int:pk>", delete_person, name="delete-person"),
 ]
